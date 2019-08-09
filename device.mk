d976880d0215748a500a87b64dc8fee9a8de2097
Rafiester <52811828+Damar131@users.noreply.github.com>
2019-08-09 15:11:00 +0700

    mido: fixing double line on device.mk

--- device.mk
+++ device.mk
@@ -121,7 +121,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     camera.msm8953 \
-    libmm-qcamera 
     libmm-qcamera \
     GoogleCameraMod \
     Snap
