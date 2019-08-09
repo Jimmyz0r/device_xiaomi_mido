a1b0ca40fcac92958e9e29cf75228249fc919432
Rafiester <52811828+Damar131@users.noreply.github.com>
2019-08-09 21:06:45 +0700

    mido: Stag-ify

--- stag_mido.mk
+++ stag_mido.mk
@@ -22,22 +22,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/mido/device.mk)
 
 
-# Inherit some common Lotus stuff
-$(call inherit-product, vendor/lotus/config/common_full_phone.mk)
-
-# Inherit MiUI camera
-$(call inherit-product, vendor/xiaomi/MiuiCamera/mido.mk)
-
-# Official
-export LOTUS_BUILD_TYPE=Official
+# Inherit some common StagOs stuff
+$(call inherit-product, vendor/stag/main.mk)
 
 #Boot Animation res
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
-PRODUCT_NAME := lotus_mido
+PRODUCT_NAME := stag_mido
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 4
 PRODUCT_MANUFACTURER := Xiaomi
